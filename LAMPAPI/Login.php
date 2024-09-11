b1c08e552d87a4d2148e4002eb20a1a077a7c5f3
FayRayyy <fay36x@gmail.com>
2024-09-11 14:01:59 -0400

    Update Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -6,17 +6,17 @@
     $firstName = "";
     $lastName = "";
 
-    $conn = new mysqli("localhost", "mariadb.sys", "Password", "contactManager");
+    $conn = new mysqli("localhost", "root", "83EuRJ+A1MJW", "contactManager");
     if( $conn->connect_error ) {
 		returnWithError( $conn->connect_error );
 	} else {
-        $stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM users WHERE Login=? AND Password=?");
+        $stmt = $conn->prepare("SELECT ID,firstName,lastName FROM users WHERE Login=? AND Password=?");
         $stmt->bind_param("ss", $inData["login"], $inData["password"]);
         $stmt->execute();
         $result = $stmt->get_result();
 
-        if( $row = $result->fetch_assoc ) {
-            returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
+        if( $row = $result->fetch_assoc() ) {
+            returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
         } else {
             returnWithError("No Records Found");
         }
