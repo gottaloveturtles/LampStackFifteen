b2d8c1880aba5935e294db9ba4ab1dae4547976a
Cora Patterson <145165542+gottaloveturtles@users.noreply.github.com>
2024-09-13 10:00:34 -0400

    Update updateContact.php

---
+++ LAMPAPI/updateContact.php
@@ -0,0 +1,52 @@
+<?php
+	$inData = getRequestInfo();
+	
+    // Attributes of each contact
+    $contactId = $inData["contactId"];
+	$userId = $inData["userId"];
+	$contactName = $inData["contactName"];
+	$email = $inData["email"];
+    $phoneNum = $inData["phoneNum"];
+
+    // Establish connection
+    $conn = new mysqli("localhost", "root", "83EuRJ+A1MJW", "contactManager");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+        // If we successfully connect, bind the parameters appropriately
+		$stmt = $conn->prepare("UPDATE Contacts SET Name=?, Email=?, Phone=? where ID = ?");
+		$stmt->bind_param("ssss", $contactName, $email, $phoneNum, $contactId);
+		
+		if ($stmt->execute())
+		{
+			returnWithError("");
+		}
+		else
+		{
+			returnWithError("Could not update contact.");
+		}
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
